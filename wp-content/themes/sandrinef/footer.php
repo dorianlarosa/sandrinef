973ee5c3db2993454d0584afa9c0a27935b4a49d
dorianlarosa <larosadorian@hotmail.fr>
2021-01-06 15:16:48 +0100

    global init + navbar

---
+++ wp-content/themes/sandrinef/footer.php
@@ -0,0 +1,11 @@
+                        <footer class="footer">
+
+                        </footer>
+
+                </main>
+
+        <?php wp_footer(); ?>
+
+        </body>
+
+</html>
---
+++ wp-content/themes/sandrinef/front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+    <div id="home-page">
+
+    </div>
+
+<?php get_footer(); ?>
